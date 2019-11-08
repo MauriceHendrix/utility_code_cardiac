ed68b27049db6180397484ca49f6b54d9f2cb6da
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2019-11-08 15:36:25 +0000

    adding manual pycmltype test

---
+++ cg_test/test/TestCg.hpp
@@ -0,0 +1,39 @@
+#ifndef TESTCG_HPP_
+#define TESTCG_HPP_
+
+#include "PyCmlLongHelperTestSuite.hpp"
+
+#include <boost/foreach.hpp>
+#include <vector>
+
+#include "HeartConfig.hpp"
+
+#include "PetscSetupAndFinalize.hpp"
+
+/**
+ * Test PyCml functionality by dynamically loading (and hence converting) a wide
+ * range of cell models.
+ *
+ * May need a test-suite setup or similar to define model-specific parameters?
+ * Should we pick up the list of models by reading the folder heart/test/data/cellml?
+ */
+class TestCg : public PyCmlLongHelperTestSuite
+{
+public:
+    void TestNormalCells()
+    {
+        std::cout << "Search for 'Failure', ': ***', 'Error', or 'Failed' to find problems." << std::endl;
+
+        std::string dirname("TestCgLongNormal");
+        std::vector<std::string> args;
+        args.push_back("--Wu");
+        std::vector<std::string> models;
+        AddAllModels(models);
+        HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.005, 0.1, 1.0);
+        RunTests(dirname, models, args);
+    }
+
+
+};
+
+#endif /*TESTCG_HPP_*/
