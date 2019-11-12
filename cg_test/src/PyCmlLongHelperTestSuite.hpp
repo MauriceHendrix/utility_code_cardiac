1dfbf7158f509743137c95df4ca1ca37b47be231
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2019-11-12 11:50:00 +0000

    cleanup test

--- cg_test/src/PyCmlLongHelperTestSuite.hpp
+++ cg_test/src/PyCmlLongHelperTestSuite.hpp
@@ -74,7 +74,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /**
  * Helper class to allow us to split the PyCmlLong tests into multiple test suites.
  */
-class PyCmlLongHelperTestSuite.hpp : public CxxTest::TestSuite
+class PyCmlLongHelperTestSuite : public CxxTest::TestSuite
 {
 
 private:
