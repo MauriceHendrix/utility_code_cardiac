49c51fbed8c2fccac82227ad7a1f9faa1de26e99
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2019-12-03 18:21:33 +0000

    trying out grandi2010ss

--- cg_test/src/CompareToPyCmlLongHelperTestSuite.hpp
+++ cg_test/src/CompareToPyCmlLongHelperTestSuite.hpp
@@ -83,8 +83,8 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "dokos_model_1996.hpp"
 #include "earm_noble_model_1990.hpp"
 #include "espinosa_model_1998_normal.hpp"
-//#include "fink_noble_giles_model_2008.hpp"
-//#include "grandi2010ss.hpp"
+#include "fink_noble_giles_model_2008.hpp"
+#include "grandi2010ss.hpp"
 //#include "hilgemann_noble_model_1987.hpp"
 #include "hodgkin_huxley_squid_axon_model_1952_modified.hpp"
 //#include "hund_rudy_2004_a.hpp"
@@ -323,12 +323,12 @@ private:
 		}else if(rModelName == "espinosa_model_1998_normal"){
 			boost::shared_ptr<AbstractCardiacCellInterface> p_cell(new Cellespinosa_model_1998_normalFromCellML(p_solver, p_stimulus));
 			return p_cell;
-//		}else if(rModelName == "fink_noble_giles_model_2008"){
-//			boost::shared_ptr<AbstractCardiacCellInterface> p_cell(new Cellfink_noble_giles_model_2008FromCellML(p_solver, p_stimulus));
-//			return p_cell;
-//		}else if(rModelName == "grandi2010ss"){
-//			boost::shared_ptr<AbstractCardiacCellInterface> p_cell(new Cellgrandi2010ssFromCellML(p_solver, p_stimulus));
-//			return p_cell;
+		}else if(rModelName == "fink_noble_giles_model_2008"){
+			boost::shared_ptr<AbstractCardiacCellInterface> p_cell(new Cellfink_noble_giles_model_2008FromCellML(p_solver, p_stimulus));
+			return p_cell;
+		}else if(rModelName == "grandi2010ss"){
+			boost::shared_ptr<AbstractCardiacCellInterface> p_cell(new Cellgrandi2010ssFromCellML(p_solver, p_stimulus));
+			return p_cell;
 //		}else if(rModelName == "hilgemann_noble_model_1987"){
 //			boost::shared_ptr<AbstractCardiacCellInterface> p_cell(new Cellhilgemann_noble_model_1987FromCellML(p_solver, p_stimulus));
 //			return p_cell;
@@ -493,8 +493,8 @@ public:
         rModels.emplace_back("dokos_model_1996");
         rModels.emplace_back("earm_noble_model_1990");
         rModels.emplace_back("espinosa_model_1998_normal");
-        //rModels.emplace_back("fink_noble_giles_model_2008");
-        //rModels.emplace_back("grandi2010ss");
+        rModels.emplace_back("fink_noble_giles_model_2008");
+        rModels.emplace_back("grandi2010ss");
         //rModels.emplace_back("hilgemann_noble_model_1987");
         rModels.emplace_back("hodgkin_huxley_squid_axon_model_1952_modified");
         //rModels.emplace_back("hund_rudy_2004_a");
