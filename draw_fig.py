2c8758ca24f74082b1d4fd7c25ffd8e372efdec2
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2021-05-21 17:09:33 +0100

    update draw fig

--- draw_fig.py
+++ draw_fig.py
@@ -3,7 +3,7 @@ from sys import float_info
 import matplotlib.pyplot as plt
 plt.rcParams['font.size'] = '20'
 
-Vmin, Vmax = -1.3356169352750066e-6, 1.3356169352750066e-6
+Vmin, Vmax = -1.5e-6, 1.5e-6#-1.3356169352750066e-6, 1.3356169352750066e-6
 
 V = Symbol('V', real=True)
 Ca_o = 1.8
@@ -14,19 +14,17 @@ Ca_i = 0.000223768331231758
 
 ICab = (-0.34100000000000003 * Ca_o + Ca_i * exp(2.0 * FonRT * V)) * F * FonRT * GCab * V / (-1.0 + exp(2.0 * FonRT * V))
 fix = (-374358.83507800003 * (-1.3356169352750065e-6 + V) * (-0.0048242499999999995 * (-0.34100000000000003 * Ca_o + Ca_i * exp(9.9999999999999995e-8)) * GCab / (-1.0 + exp(9.9999999999999995e-8)) - 0.0048242499999999995 * (-0.34100000000000003 * Ca_o + Ca_i * exp(-9.9999999999999995e-8)) * GCab / (-1.0 + exp(-9.9999999999999995e-8))) + 0.0048242499999999995 * (-0.34100000000000003 * Ca_o + Ca_i * exp(9.9999999999999995e-8)) * GCab / (-1.0 + exp(9.9999999999999995e-8)))
-#fix = 0.000884254431877711*V - 0.0236221547699112
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.tight_layout(pad=0.5)
 
-#print(ICab.replace(V,0.0))
-#assert False
 
 x, y, x2, y2 = [], [], [], []
 current = -80
 while current < 80 and current < Vmin:
     x.append(current)
     y.append(ICab.replace(V, current))
-    current += 1/10
+    current += (80-Vmin) /1000
 
 
 current = Vmin
@@ -37,39 +35,41 @@ while current <= Vmax:
         x.append(current)
         y.append(val)
     except TypeError:
-        pass
-        x2.append(current)
-        y2.append(fix.replace(V, current))
+        if (0.0748717670156 * current>= -9.9999999999999995e-8) and (0.0748717670156 *current <= 9.9999999999999995e-8):
+            x2.append(current)
+            y2.append(fix.replace(V, current))
         x.append(current)
         x.append(current)
         y.append(limit(ICab, V, current, dir='-').replace(oo,100).replace(-oo, -100))
         y.append(limit(ICab, V, current, dir='+').replace(oo,100).replace(-oo, -100))
 
-    x2.append(current)
-    y2.append(fix.replace(V, current))
+    if (0.0748717670156 * current>= -9.9999999999999995e-8) and (0.0748717670156 *current <= 9.9999999999999995e-8):
+        x2.append(current)
+        y2.append(fix.replace(V, current))
     current += (Vmax - Vmin) / 10000
     
 current = Vmax
 while current < 80:
     x.append(current)
     y.append(ICab.replace(V, current))
-    current += 1/10
+    current += (80-Vmin) /1000
 
 
-#plt.xlim(float(Vmin - (Vmax - Vmin)), float(Vmax + (Vmax - Vmin)))
 ax1.plot(x,y, color="blue")
 ax1.plot(x2,y2, color="red")
 ax1.set_ylim(-0.15, 0.005)
-ax1.set_ylabel('ICab', fontsize=25)
-ax1.set_xlabel('V', fontsize=25)
+ax1.set_ylabel('$I_{Ca,b}$ (A / F)', fontsize=25)
+ax1.set_xlabel('V (mV)', fontsize=25)
 
 
 ax2.plot(x,y, color="blue")
 ax2.plot(x2,y2, color="red")
-ax2.set_ylim(-0.023622155965, -0.023622153531)
+#ax2.set_ylim(-0.023622155965-1e-9, -0.023622153531+1e-9)
+ax2.ticklabel_format( axis='y', useOffset=False)
+ax2.set_ylim(-0.023622155965-1e-9, -0.023622153531+1e-9)
 ax2.set_xlim(-1.5*1e-6, 1.5*1e-6)
-ax2.set_ylabel('ICab', fontsize=25)
-ax2.set_xlabel('V', fontsize=25)
+ax2.set_ylabel('$I_{Ca,b}$ (A / F)', fontsize=25)
+ax2.set_xlabel('V (mV)', fontsize=25)
 
 
 plt.show()
